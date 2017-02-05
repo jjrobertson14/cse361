476e858bee73b6941022e7a0698a8b7c7190a503
jjrobertson <jjrobertson@oakland.edu>
2017-02-05 17:43:57 -0500

    starting project 2

---
+++ src/projects/Tree.java
@@ -0,0 +1,9 @@
+package projects;
+import java.util.Iterator;
+/**
+ * Created by jjrobertson on 2/5/2017.
+ */
+public class Tree {
+    TreeSet<Node>;
+    Nod
+}
---
+++ src/projects/TreeTest.java
@@ -0,0 +1,7 @@
+package projects;
+
+/**
+ * Created by 1234r_000 on 2/5/2017.
+ */
+public class TreeTest {
+}
