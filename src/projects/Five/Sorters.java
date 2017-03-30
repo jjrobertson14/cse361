53fb489e143e9b35a6ce3150cefdc0dd1021cbd6
jjrobertson <jjrobertson@oakland.edu>
2017-03-29 23:28:00 -0400

    project5 work

--- src/projects/Five/Sorters.java
+++ src/projects/Five/Sorters.java
@@ -10,11 +10,34 @@ public class Sorters {
 
     }
 
-    public void mergeSort(int[] A) {
-        
+    /*
+     * in place mergesort
+     */
+    public void mergeSort(int[] A, int a, int b) {
+        int n = b - a + 1;
+        int m = (int)Math.ceil(n/2);
+        if (n == 1) {
+            return
+        }
     }
 
+    private int[] merge(int[] A, int a, int m, int b) {
+        while (a < b) {
+            if (A[a] < A[b]) {
+                a++;
+            } else {
+                //put b into a, shift elements to right
+                A[a] += A[b];
+                A[b] = A[a] - A[b];
+                A[a] = A[a] - A[b];
 
+            }
+        }
+    }
+
+    /* 1 2 4 5  | 8 9 6 10
+                  a   b
+     */
     public void heapSort(int[] A) {
         PriorityQueue<Integer> PQ = new PriorityQueue<>();
         for (int i = 0; i < A.length; i++) {
